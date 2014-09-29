c443f353aed37da4d9c566c89b2d90ed8826055e
Tom Caflisch <tomcaflisch@gmail.com>
2014-09-29 16:13:37 -0500

    Made seed command only look at .json files in seeds folder

--- test/test.js
+++ test/test.js
@@ -8,26 +8,37 @@ var exec = require('child_process').exec;
 var path = require('path');
 
 describe('run seed-setup', function () {
-  it.only('creates the seed.json file', function (done) {
+
+	it('creates the seed.json file', function (done) {
     child = exec('cd test/temp; node ../../bin/setup', function(err, stdout, stderr) {
 			expect(err).to.be.null;
 			expect(path.resolve('./test/temp/seed.json')).to.be.a.file();
+			console.log(stdout);
       done();
-    })
+    });
   });
+
 });
 
 describe('run seed', function () {
-  it('fails with no seeds folder', function () {
 
+  it('fails with no seeds folder', function (done) {
+		child = exec('cd test/temp; node ../../bin/seed', function(err, stdout, stderr) {
+			expect(err).to.be.null;
+			//expect(path.resolve('./test/temp/seed.json')).to.be.a.file();
+			console.log(stdout);
+			// Need to assert something here
+			done();
+		});
   });
 
   it('runs successfully with a seeds folder', function (done) {
 		child = exec('cd test/temp; node ../../bin/seed', function(err, stdout, stderr) {
 			expect(err).to.be.null;
+			console.log(stdout);
 			// Need to assert something else here
+			done();
 		})
   });
-
 
 });
