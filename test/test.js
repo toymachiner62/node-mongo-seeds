0500b6d495524a5ebd3efc98fefdb57132473ac9
Tom Caflisch <tomcaflisch@gmail.com>
2018-06-24 16:28:20 -0700

    :construction_worker: swapped out Q for natice Promises :muscle:

--- test/test.js
+++ test/test.js
@@ -1,18 +1,18 @@
 /**
  * Remeber to run from the project root or the tests will fail!
  */
-var chai    = require('chai');
-var expect  = chai.expect;
+const chai    = require('chai');
+const expect  = chai.expect;
 chai.use(require('chai-fs'));
-var exec    = require('child_process').exec;
-var path    = require('path');
+const exec    = require('child_process').exec;
+const path    = require('path');
 
 describe('run seed-setup', function () {
 
   it('creates the seed.json file', function (done) {
-    child = exec('cd test/empty; node ../../bin/setup', function(err, stdout, stderr) {
+    exec('cd test/empty; node ../../bin/setup', function(err, stdout, stderr) {
       expect(err).to.be.null;
-      expect(path.resolve('./test/empty/seed.json')).to.be.a.file();
+      expect(path.resolve('./test/empty/seed.js')).to.be.a.file();
       exec('rm -f test/empty/seed.json');
       done();
     });
@@ -23,7 +23,7 @@ describe('run seed-setup', function () {
 describe('run seed', function () {
 
   it('fails with no seeds folder', function (done) {
-    child = exec('cd test/invalid; node ../../bin/seed', function (err, stdout, stderr) {
+    exec('cd test/invalid; node ../../bin/seed', function (err, stdout, stderr) {
       expect(err).to.be.null;
       // Give a reason why
       expect(stdout).to.match(/Error/);
@@ -32,7 +32,7 @@ describe('run seed', function () {
   });
 
   it('filters out non-json from the seeds folder', function (done) {
-    child = exec('cd test/filter; node ../../bin/seed', function (err, stdout, stderr) {
+    exec('cd test/filter; node ../../bin/seed', function (err, stdout, stderr) {
       expect(err).to.be.null;
       // This is a cheap way to check that we did something
       expect(stdout).to.match(/Seeding collection testing/);
@@ -42,14 +42,8 @@ describe('run seed', function () {
   });
 
   describe('with no NODE_ENV set', function() {
-
-    it.only('runs successfully with a seeds folder', function (done) {
-      child = exec('cd test/valid; node ../../bin/seed', function(err, stdout, stderr) {
-
-        console.log(err)
-        console.log(stdout)
-
-
+    it('runs successfully with a seeds folder', function (done) {
+      exec('cd test/valid; node ../../bin/seed', function(err, stdout, stderr) {
         expect(err).to.be.null;
         // This is a cheap way to check that we did something
         expect(stdout).to.match(/Seeding collection testing/);
@@ -62,9 +56,8 @@ describe('run seed', function () {
   describe('with NODE_ENV set and with a matching key in seed.json', function() {
 
     describe('not specifying the mongodb protocol', function() {
-
       it('runs successfully with a seeds folder', function (done) {
-        child = exec('cd test/protocol; NODE_ENV=dev node ../../bin/seed', function(err, stdout, stderr) {
+        exec('cd test/protocol; NODE_ENV=dev node ../../bin/seed', function(err, stdout, stderr) {
           expect(err).to.be.null;
           // This is a cheap way to check that we did something
           expect(stdout).to.match(/Seeding collection testing/);
@@ -75,9 +68,8 @@ describe('run seed', function () {
     });
 
     describe('explicitly specifying the mongodb protocol', function() {
-
       it('runs successfully with a seeds folder', function (done) {
-        child = exec('cd test/valid; NODE_ENV=dev node ../../bin/seed', function(err, stdout, stderr) {
+        exec('cd test/valid; NODE_ENV=dev node ../../bin/seed', function(err, stdout, stderr) {
           expect(err).to.be.null;
           // This is a cheap way to check that we did something
           expect(stdout).to.match(/Seeding collection testing/);
@@ -90,9 +82,8 @@ describe('run seed', function () {
   });
 
   describe('using MongoDB Extended JSON format', function() {
-
     it('tests $date format', function (done) {
-      child = exec('cd test/validExtended; node ../../bin/seed', function(err, stdout, stderr) {
+      exec('cd test/validExtended; node ../../bin/seed', function(err, stdout, stderr) {
           expect(err).to.be.null;
 
           // This is a cheap way to check that we did something
@@ -104,9 +95,8 @@ describe('run seed', function () {
   });
 
   describe('with NODE_ENV set and no matching key in seed.json', function() {
-
     it('errors out with a reason why', function (done) {
-      child = exec('cd test/valid; NODE_ENV=test node ../../bin/seed', function(err, stdout, stderr) {
+      exec('cd test/valid; NODE_ENV=test node ../../bin/seed', function(err, stdout, stderr) {
         expect(err).to.be.null;
         expect(stdout).to.match(/No key exists in seed.json for the passed in NODE_ENV/);
         done();
